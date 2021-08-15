5f7ce7d6ac2d8d7bd8011b3c435ce9775c78000c
velrino <denismagalhaesvelrino@gmail.com>
2021-08-15 20:08:27 -0300

    :tada: Started project

---
+++ src/test/java/com/social/networking/socialnetworking/SocialnetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.networking.socialnetworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
